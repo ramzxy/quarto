087a8b5e7c551771e569cc6baf9b11b1d841ba27
GregoryJaperson <89696741+GregoryJaperson@users.noreply.github.com>
2026-01-20 17:02:00 +0100

    Created template for Game.java

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,4 +1,51 @@
 package Game;
 
+import java.util.List;
+
 public class Game {
+    private Board board;
+    private List<Piece> availablePieces;
+    private Piece currentPieceToPlace;
+    private AbstractPlayer[] playerList;
+    private int currentTurn;
+    private List<GameListener> gameListeners;
+
+
+    /**
+     * Main function to be called when starting a particular game.
+     */
+    public void start(){
+
+    }
+
+    /**
+     * Function to do a move.
+     * @param move The move to be played
+     */
+    public void doMove(Move move){
+
+    }
+
+    /**
+     * Gives all valid moves that can be played in the current state of the game.
+     * @return List of all moves that can be played
+     */
+    public List<Move> getValidMoves(){
+        return null;
+    }
+
+    /**
+     * Function to add a game listener to the game.
+     * @param gameListener Implementation of GameListener to be added
+     */
+    public void addListener(GameListener gameListener){
+
+    }
+
+    /**
+     * Notifies all listeners to the game about the current state of the game.
+     */
+    public void notifyListeners(){
+
+    }
 }
